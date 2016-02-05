d916d051ad1b5851d6b4bfe744adada80e967bc5
chhe <chhe@ebay.com>
2016-02-05 17:24:33 -0500

    commit

--- PickerOne/PickerOne/MainCollectionViewCells.swift
+++ PickerOne/PickerOne/MainCollectionViewCells.swift
@@ -12,6 +12,11 @@ import UIKit
 
 class MainCollectionViewCells: UICollectionViewCell {
     
+    @IBOutlet var titleLabel:UILabel?
+    @IBOutlet var numberOfChoicesLabel:UILabel?
+    @IBOutlet var pick1Button:UIButton?
+    
+    var pickJob:PickerJob?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,6 +25,16 @@ class MainCollectionViewCells: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         //fatalError("init(coder:) has not been implemented")
         super.init(coder: aDecoder)
+    }
+    
+    func loadContent(){
+       if let title = pickJob?.title{
+            titleLabel?.text = title
+        }
+        
+        if let numberOfChoices = pickJob?.jobDetails.count{
+            numberOfChoicesLabel?.text =  "\(numberOfChoices) total choices"
+        }
     }
     
 //    func loadContent(completionHandler:(DailyReading?,ErrorType?)->Void)
